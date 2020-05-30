7c3960d355cdf223315069842b55fdcdeae4ed93
Jeff <sagalbot@gmail.com>
2020-05-30 15:58:54 -0700

    start of image gallery

---
+++ src/components/ImageCard.vue
@@ -0,0 +1,23 @@
+<template functional>
+  <img
+    :src="props.src"
+    :alt="props.alt"
+    :title="props.alt"
+    class="border border-gray-200 rounded-lg"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      required: true,
+      type: String
+    },
+    alt: {
+      required: true,
+      type: String
+    }
+  }
+};
+</script>
--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,11 +1,47 @@
 <template>
   <div>
-    <img src="" alt="" />
+    <ImageCard :src="currentImage.src" :alt="currentImage.alt" />
+    <nav>
+      <ul class="flex flex-row justify-between">
+        <li v-for="({ src, alt }, key) in images" :key="key">
+          <ImageCard :src="src" :alt="alt" />
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
+import ImageCard from "@/components/ImageCard.vue";
+const images = {
+  front: {
+    src: "/img/kemper-front.jpg",
+    alt:
+      "Kemper Profiling amplifier viewed from the front, displaying the unit controls."
+  },
+  angle: {
+    src: "/img/kemper-angle.jpg",
+    alt:
+      "Kemper Profiling amplifier viewed from an angle, displaying the unit controls, handle, and shape."
+  },
+  rear: {
+    src: "/img/kemper-rear.jpg",
+    alt:
+      "Kemper Profiling amplifier viewed from an rear, displaying the unit connection inputs and outputs."
+  }
+};
+
 export default {
-  name: "ImageGallery"
+  name: "ImageGallery",
+  components: { ImageCard },
+  data: () => ({
+    currentImageIndex: "front"
+  }),
+  computed: {
+    images: () => images,
+    currentImage() {
+      return images[this.currentImageIndex];
+    }
+  }
 };
 </script>
