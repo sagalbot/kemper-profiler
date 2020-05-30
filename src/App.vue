fe946268045bdd79676d4bf8d5f7009723408d67
Jeff <sagalbot@gmail.com>
2020-05-30 14:42:11 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div id="app"></div>
+  <article>
+    <header class="border-b border-gray-300 mb-8 pb-4">
+      <h1 class="font-extrabold text-gray-900 text-4xl tracking-tight">
+        Get your Kemper Profiling Amp
+      </h1>
+      <p class="text-gray-500 text-xl leading-normal">
+        All your favorite amps and effects, together in one little box.
+      </p>
+    </header>
+  </article>
 </template>
 
 <script>
 export default {
-  name: "App",
-  components: {}
+  name: "App"
 };
 </script>
