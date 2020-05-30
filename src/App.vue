31a1e28a545bc6955e7bbcb9c181c2f6714becfa
Jeff <sagalbot@gmail.com>
2020-05-30 15:12:30 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <article
     class="px-4 pt-12 sm:px-12 sm:pt-18 md:px-28 lg:px-8 xl:px-28 max-w-screen-xl"
   >
-    <header class="border-b border-gray-300 mb-8 pb-4">
+    <header class="border-b border-gray-300 mb-8">
       <h1
-        class="font-extrabold text-gray-900 text-4xl tracking-tight leading-tight"
+        class="font-extrabold text-gray-900 text-4xl tracking-tight leading-tight mb-4 sm:mb-3"
       >
         Get your Kemper Profiling Amp
       </h1>
-      <p class="text-gray-500 text-xl leading-normal">
+      <p class="text-gray-500 text-xl leading-normal mb-4 sm:mb-5 md:mb-6">
         All your favorite amps and effects, together in one little box.
       </p>
     </header>
