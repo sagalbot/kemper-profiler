394d4406b0ae6ad224483c22b5c1bae5658a7265
Jeff <sagalbot@gmail.com>
2020-06-06 13:32:24 -0700

    bit more complicated than I'd like it to be

--- src/App.vue
+++ src/App.vue
@@ -18,40 +18,64 @@
         class="lg:w-1/2 lg:sticky top-0 mb-8 lg:mb-0 lg:mr-8 flex-grow-0"
       />
       <div class="lg:w-1/2">
-        <h2 class="flex flex-col mb-6">
-          <span class="font-medium text-sm text-gray-500 tracking-normal mb-1"
-            >Starting at</span
-          >
-          <span
-            class="tracking-tight leading-none text-gray-900 text-5xl font-extrabold"
-            >$1,799</span
-          >
-        </h2>
-        <p class="mb-4 text-lg text-gray-900">
-          The KEMPER PROFILER™ is the leading-edge digital guitar amplifier and
-          all-in-one effects processor.
-        </p>
-        <p class="mb-4 text-gray-500">
-          Hailed as a game-changer by guitarists the world over, the PROFILER™
-          is the first digital guitar amp to really nail the full and dynamic
-          sound of a guitar or bass amp.
-        </p>
-        <p class="mb-4 text-gray-500">
-          This is made possible by a radical, patented technology and concept
-          which we call "PROFILING".
-        </p>
+        <div class="mb-12">
+          <h2 class="flex flex-col mb-6">
+            <span
+              class="font-medium text-sm text-gray-500 tracking-normal mb-1"
+            >
+              Starting at
+            </span>
+            <span
+              class="tracking-tight leading-none text-gray-900 text-5xl font-extrabold"
+            >
+              $1,799
+            </span>
+          </h2>
 
-        <div v-for="{ title, options } in configurationOptions" :key="title">
-          <h3>{{ title }}</h3>
-          <ul>
-            <li v-for="{ label, body, upcharge } in options" :key="label">
+          <p class="mb-4 text-lg text-gray-900">
+            The KEMPER PROFILER™ is the leading-edge digital guitar amplifier
+            and all-in-one effects processor.
+          </p>
+          <p class="mb-4 text-gray-500">
+            Hailed as a game-changer by guitarists the world over, the PROFILER™
+            is the first digital guitar amp to really nail the full and dynamic
+            sound of a guitar or bass amp.
+          </p>
+          <p class="text-gray-500">
+            This is made possible by a radical, patented technology and concept
+            which we call "PROFILING".
+          </p>
+        </div>
+
+        <div
+          v-for="{ title, options, classes } in configurationOptions"
+          :key="title"
+          class="mb-12"
+        >
+          <h3 class="mb-2 font-medium text-lg text-gray-900">{{ title }}</h3>
+          <ol class="space-y-4" :class="classes || ''">
+            <Selectable
+              v-for="{ label, body, upCharge, classes } in options"
+              tag="li"
+              class="px-6 py-5 cursor-pointer flex justify-between items-center flex-1"
+              :class="classes || ''"
+              :key="label"
+              :selected="isOptionSelected(title, label)"
+              :default-border-shade="300"
+              @click="selectOption(title, label)"
+              @keydown.space="selectOption(title, label)"
+            >
               <div>
-                <h4>{{ label }}</h4>
-                <p>{{ body }}</p>
+                <h4 class="text-sm font-medium" :class="{ 'mb-2': body }">
+                  {{ label }}
+                </h4>
+                <p v-if="body" class="text-gray-500 text-sm">{{ body }}</p>
               </div>
-              <p v-if="upcharge">+ ${{ upcharge }}</p>
-            </li>
-          </ul>
+              <p v-if="upCharge" class="text-sm flex-shrink-0">
+                + ${{ upCharge }}
+              </p>
+            </Selectable>
+          </ol>
         </div>
       </div>
     </div>
@@ -60,34 +84,37 @@
 
 <script>
 import ImageGallery from "./components/ImageGallery.vue";
+import Selectable from "./components/Selectable";
+
 export default {
   name: "App",
-  components: { ImageGallery },
+  components: { Selectable, ImageGallery },
   data: () => ({
     selectedConfiguration: {
       "Form Factor": "Profiler Head",
       "Power Amp": "None",
-      "Foot Controller": "None",
-    },
+      "Foot Controller": "None"
+    }
   }),
   computed: {
     configurationOptions() {
       return [
         {
           title: "Form Factor",
+          classes: "sm:flex sm:justify-evenly sm:space-y-0 sm:space-x-4",
           options: [
             {
               label: "Profiler Head",
               body:
                 "Compact amplifier head, perfect for a speaker cabinet or desk.",
-              upcharge: 0,
+              upCharge: 0
             },
             {
               label: "Profiler Rack",
               body: "3U rackmount version of the classic profiling amplifier.",
-              upcharge: 0,
-            },
-          ],
+              upCharge: 0
+            }
+          ]
         },
         {
           title: "Power Amp",
@@ -95,14 +122,14 @@ export default {
             {
               label: "None",
               body: "Use in the studio or with your own power amp.",
-              upcharge: 0,
+              upCharge: 0
             },
             {
               label: "Powered",
               body: "Built-in 600W solid state power amp.",
-              upcharge: 449,
-            },
-          ],
+              upCharge: 449
+            }
+          ]
         },
         {
           title: "Foot Controller",
@@ -110,17 +137,25 @@ export default {
             {
               label: "None",
               body: false,
-              upcharge: 0,
+              upCharge: 0
             },
             {
               label: "Profiler Remote Foot Controller",
-              body: "3U rackmount version of the classic profiling amplifier.",
-              upcharge: 449,
-            },
-          ],
-        },
+              body: false,
+              upCharge: 449
+            }
+          ]
+        }
       ];
-    },
+    }
   },
+  methods: {
+    isOptionSelected(title, label) {
+      return this.selectedConfiguration[title] === label;
+    },
+    selectOption(title, label) {
+      return (this.selectedConfiguration[title] = label);
+    }
+  }
 };
 </script>
